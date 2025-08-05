83f31af58ef8e08a7a029f61fd292a9d7fbafb02
Knight walker <41152282+nirajnagrale@users.noreply.github.com>
2025-08-05 18:53:57 +0000

    added go ut

---
+++ test/messageQueue_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"ordercraft/pkg/messageQueue"
+	"ordercraft/pkg/vectorClock"
+	"testing"
+)
+
+func TestMessageQueue(t *testing.T) {
+	// Create a new message queue
+	mq := messageQueue.NewMessageQueue()
+	// Create vector clocks for testing
+	vc1 := vectorClock.NewVectorClock("node1")
+	vc2 := vectorClock.NewVectorClock("node2")
+
+	// Create queue nodes with messages
+	vc1_snapshot := vc1.GetSnapshot()
+	vc2_snapshot := vc2.GetSnapshot()
+
+	node1 := messageQueue.GetQueueItem(&vc1_snapshot, "Message from node1")
+	node2 := messageQueue.GetQueueItem(&vc2_snapshot, "Message from node2")
+
+	// Enqueue messages
+	mq.Enqueue(node1)
+	mq.Enqueue(node2)
+	// Check the size of the queue for node1
+	if len(mq.Nodes["node1"]) != 1 {
+		t.Errorf("Expected 1 message in node1's queue, got %d", len(mq.Nodes["node1"]))
+	}
+	// Check the size of the queue for node2
+	if len(mq.Nodes["node2"]) != 1 {
+		t.Errorf("Expected 1 message in node2's queue, got %d", len(mq.Nodes["node2"]))
+	}
+	// Dequeue messages
+	dequeuedNode1, exists1 := mq.Dequeue("node1")
+	if !exists1 || dequeuedNode1.Msg != "Message from node1" {
+		t.Errorf("Expected to dequeue 'Message from node1', got %s", dequeuedNode1.Msg)
+	}
+	dequeuedNode2, exists2 := mq.Dequeue("node2")
+	if !exists2 || dequeuedNode2.Msg != "Message from node2" {
+		t.Errorf("Expected to dequeue 'Message from node2', got %s", dequeuedNode2.Msg)
+	}
+	// Check if the queues are empty after dequeueing
+	if len(mq.Nodes["node1"]) != 0 {
+		t.Errorf("Expected node1's queue to be empty, got %d", len(mq.Nodes["node1"]))
+	}
+
+}
---
+++ test/vectorClock_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"ordercraft/pkg/vectorClock"
+	"testing"
+)
+
+func TestVectorClock(t *testing.T) {
+	// Initialize a new vector clock
+	vc := vectorClock.NewVectorClock("node1")
+
+	// Tick the vector clock
+	snapshot, err := vc.Tick()
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the snapshot id
+	println("Snapshot after tick:")
+	println("Snapshot ID:", snapshot.Id)
+	//print the snapshot values
+	for id, value := range snapshot.Clock {
+		println("Node:", id, "Value:", value)
+	}
+	// Add a new node
+	vc.AddNode("node2")
+	println("Added node2 to vector clock")
+
+	// Tick again
+	snapshot, err = vc.Tick()
+	if err != nil {
+		panic(err)
+	}
+	println("Snapshot after adding node2:")
+	println("Snapshot after tick:", snapshot.Id)
+	//print the snapshot values
+	for id, value := range snapshot.Clock {
+		println("Node:", id, "Value:", value)
+	}
+	// Delete a node
+	vc.DeleteNode("node1")
+	println("Deleted node1 from vector clock")
+}
